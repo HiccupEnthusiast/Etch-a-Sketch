f0072332f36c829e407ead5190aa6a6d1d76c61c
Hicster Enthusiast <hicster.enthusiast@gmail.com>
2022-06-09 19:54:48 -0500

    Reset button properly functioning

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const grid = document.querySelector('.grid');
+let grid = document.querySelector('.grid');
+const resetBtn = document.querySelector('button.restart');
 
-createGrid(16, 16);
+let x = 16;
+let y = 16;
+
+resetBtn.addEventListener('click', () => {
+    y = Number(prompt("Insert number of rows: ", '16'));
+    x = Number(prompt('Insert number of columns: ', '16'));
+    if (!isNaN(x) && !isNaN(y)){
+        clearGrid();
+        createGrid(x, y);
+    } else {
+        alert('Invalid input!');
+    }
+});
+
+createGrid(x, y);
 
 function createGrid(rows, columns) {
     for (let i = 1; i <= rows; i++) {
@@ -16,6 +31,15 @@ function createGrid(rows, columns) {
         grid.appendChild(column);
     };    
 };
+
+function clearGrid() {
+    const container = document.querySelector('.container');
+    
+    grid.remove();
+    grid = document.createElement('div');
+    grid.className = "grid";
+    container.appendChild(grid);
+}
 
 function toggleColor() {
     this.classList.toggle("color-toggle");
